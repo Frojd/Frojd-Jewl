e6d4fd36816a0e6ec8e7eff67679c96948cae1f3
saraojelind <sara.ojelind@frojd.se>
2019-08-16 10:23:19 +0200

    Add velocity react and change accordian

--- frontend/app/components/Accordian/Accordian.js
+++ frontend/app/components/Accordian/Accordian.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { VelocityTransitionGroup } from 'velocity-react';
 
 import RawHtml from 'Components/RawHtml';
 
 import './Accordian.scss';
 
-const Accordian = ({title, richtext, open}) => (
-    <div className="Accordian">
-        <div className="Accordian__Header">
-            {title}
+const Accordian = ({title, richtext, open}) => {
+
+    const [expanded, setExpanded] = useState(open);
+
+    const classes = classNames(
+        'Accordian',
+        { 'Accordian--Expanded' : expanded }
+    );
+
+    const clickHandler = () => setExpanded(!expanded);
+
+    return (
+        <div className={classes}>
+            <div className="Accordian__Header" onClick={clickHandler}>
+                {title}
+            </div>
+            <VelocityTransitionGroup
+                enter={{animation: "slideDown"}}
+                leave={{animation: "slideUp"}}
+            >
+                {expanded &&
+                    <div className="Accordian__Content">
+                        <RawHtml html={richtext} />
+                    </div>
+                }
+            </VelocityTransitionGroup>
         </div>
-        <div className="Accordian__Content">
-            <RawHtml html={richtext} />
-        </div>
-    </div>
-);
+    );
+}
 
 Accordian.propTypes = {
     title: PropTypes.string.isRequired,
