0c4136df478a3fb6e2cbc965044afe84b8fe4993
saraojelind <sara.ojelind@frojd.se>
2019-08-16 16:31:46 +0200

    Change accordian for accessibility

--- frontend/app/components/Accordian/Accordian.js
+++ frontend/app/components/Accordian/Accordian.js
@@ -7,7 +7,7 @@ import RawHtml from 'Components/RawHtml';
 
 import './Accordian.scss';
 
-const Accordian = ({title, richtext, open}) => {
+const Accordian = ({title, richtext, open, id}) => {
 
     const [expanded, setExpanded] = useState(open);
 
@@ -18,15 +18,28 @@ const Accordian = ({title, richtext, open}) => {
 
     const clickHandler = () => setExpanded(!expanded);
 
+    const headerId = `${id}-header`;
+
     return (
         <div className={classes}>
-            <div className="Accordian__Header" onClick={clickHandler}>
+            <div
+                className="Accordian__Header"
+                onClick={clickHandler}
+                aria-expanded={expanded}
+                aria-controls={id}
+                id={headerId}
+            >
                 {title}
             </div>
             <VelocityComponent
                 animation={expanded ? 'slideDown' : 'slideUp'}
             >
-                <div className="Accordian__Content">
+                <div
+                    className="Accordian__Content"
+                    aria-hidden={!expanded}
+                    aria-labelledby={headerId}
+                    id={id}
+                >
                     <RawHtml html={richtext} />
                 </div>
             </VelocityComponent>
@@ -38,12 +51,14 @@ Accordian.propTypes = {
     title: PropTypes.string.isRequired,
     richtext: PropTypes.string.isRequired,
     open: PropTypes.bool,
+    id: PropTypes.string,
 };
 
 Accordian.defaultProps = {
     title: '',
     richtext: '',
     open: false,
+    id: 'accordian',
 };
 
 export default Accordian;
