45b2ce030ac66c31b8a1a444ce5df4d11487036e
saraojelind <sara.ojelind@frojd.se>
2019-11-01 14:16:37 +0100

    Change RawHtml

--- frontend/app/components/RawHtml/RawHtml.js
+++ frontend/app/components/RawHtml/RawHtml.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './RawHtml.scss';
+import s from './RawHtml.scss';
 
 const RawHtml = ({ html }) => (
-    <div className="RawHtml" dangerouslySetInnerHTML={{ __html: html }} />
+    <div className={s.Root} dangerouslySetInnerHTML={{ __html: html }} />
 );
 
 RawHtml.propTypes = {
