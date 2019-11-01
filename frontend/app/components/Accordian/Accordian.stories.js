76fd51af1a8a458f5ef3465de80a39dbfae138dd
saraojelind <sara.ojelind@frojd.se>
2019-11-01 11:01:53 +0100

    Add different examples of accordian and change documentation

--- frontend/app/components/Accordian/Accordian.stories.js
+++ frontend/app/components/Accordian/Accordian.stories.js
@@ -1,14 +1,45 @@
 import * as React from 'react';
+import {
+  withKnobs,
+  text,
+} from '@storybook/addon-knobs';
 
 import Accordian from './Accordian';
 import markdown from './Accordian.md';
 import data from './Accordian.data';
 
-export default { title: 'Components|Accordian'}
+export default {
+    title: 'Components|Accordian',
+    decorators: [withKnobs],
+};
 
-export const withData = () => <Accordian {...data} />;
-withData.story = {
+export const emptyComp = () => <Accordian />;
+
+export const dataComp = () => <Accordian {...data} />;
+
+export const dynamicComp = () => (
+    <Accordian
+        title={text('Title', data.title)}
+        richtext={text('Richtext', data.richtext)}
+    />
+);
+
+emptyComp.story = {
+    name: 'Empty',
     parameters: {
         notes: { markdown: markdown },
+    },
+};
+dataComp.story = {
+    name: 'With data',
+    parameters: {
+        notes: { markdown: markdown },
+    },
+};
+dynamicComp.story = {
+    name: 'Dynamic',
+    parameters: {
+        notes: { markdown: markdown },
+        knobs: { escapeHTML: false },
     },
 };
