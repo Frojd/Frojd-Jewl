da43e84f6038be02028911a7732332739f81d4d2
saraojelind <sara.ojelind@frojd.se>
2021-05-25 08:07:48 +0200

    Change i18n

--- component-library/app/i18n/index.js
+++ component-library/app/i18n/index.js
@@ -1,32 +1,31 @@
-/**
- * Usage:
- * import i18n from 'i18n'
- *
- * then call translate like this:
- * i18n.t('hello.world', 'Fallback')
- *
- * See: https://www.i18next.com/ for more information
- */
+import i18n from 'i18next';
+// import Backend from 'i18next-xhr-backend';
+import { initReactI18next } from 'react-i18next';
+import sv from './translations/sv';
 
-import i18next from 'i18next';
+i18n
+  // load translation using xhr -> see /public/locales
+  // learn more: https://github.com/i18next/i18next-xhr-backend
+  // .use(Backend)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    lng: 'sv',
+    fallbackLng: 'sv',
+    debug: false,
 
-import sv from './translations/sv.json';
-
-i18next.init({
-    languages: ['sv'],
-    fallbackLng: ['sv'],
-    resources: {
-        sv: {
-            translation: sv,
-        },
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
     },
-});
 
-// Update this to reflect what language your page should use
-const lang = () => {
-    return 'sv';
-};
+    resources: {
+      sv: {
+        translation: sv,
+      }
+    }
+  });
+  i18n.addResourceBundle('sv', 'translation', sv, true, true);
 
-i18next.changeLanguage(lang());
-
-export default i18next;
+export default i18n;
---
+++ component-library/app/i18n/translations/sv.js
@@ -0,0 +1,5 @@
+export default {
+    header: {
+        search: 'Sök',
+    },
+};
