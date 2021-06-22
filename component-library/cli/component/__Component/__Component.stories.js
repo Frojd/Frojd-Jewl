c3bc849c8aeddcedffcdab251c1afa8dc9b7dae5
saraojelind <sara.ojelind@frojd.se>
2021-06-22 22:21:02 +0200

    Change stories template for creating new components and containers

--- component-library/cli/component/__Component/__Component.stories.js
+++ component-library/cli/component/__Component/__Component.stories.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Description} from '@storybook/addon-docs/blocks';
 import __Component from './__Component';
 import data from './__Component.data';
+import readme from './__Component.md';
 
-export default { title: '__StoryTitle' };
+const Doc = () => <Description markdown={readme} />;
+
+export default {
+    title: '__StoryTitle',
+    parameters: {
+        docs: {
+            page: Doc
+        }
+    },
+};
 
 export const __ComponentDefault = () => (
     <__Component {...data} />
--- component-library/cli/component/__Container/__Container.stories.js
+++ component-library/cli/component/__Container/__Container.stories.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Description} from '@storybook/addon-docs/blocks';
 import __Container from './__Container';
 import data from './__Container.data';
+import readme from './__Container.md';
 
-export default { title: '__StoryTitle' };
+const Doc = () => <Description markdown={readme} />;
+
+export default {
+    title: '__StoryTitle',
+    parameters: {
+        docs: {
+            page: Doc
+        }
+    },
+};
 
 export const __ContainerDefault = () => (
     <__Container {...data} />
