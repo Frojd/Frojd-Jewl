008f35e58e92e0442910737ed2677af3d3f27bfc
mien <mikael.pj.engstrom@gmail.com>
2021-06-29 18:42:52 +0200

    Make init job clone local repository

--- cli/src/commands/init.ts
+++ cli/src/commands/init.ts
@@ -1,7 +1,8 @@
 import {Command, flags} from '@oclif/command'
 import cli from 'cli-ux'
-import {CONFIG_FILE_NAME} from '../constants'
-import {loadJSON} from "@oclif/config/lib/util";
+import {CONFIG_FILE_NAME, REPO_PATH} from '../constants'
+import {loadJSON} from "@oclif/config/lib/util"
+import {Clone, Repository} from 'nodegit'
 var fs = require('fs')
 
 export default class Init extends Command {
@@ -13,7 +14,10 @@ export default class Init extends Command {
 
   async run() {
     let config = {
-      componentPath: "app/components"
+      componentPath: "app/components",
+      repository: "git@github.com:Frojd/Frojd-Jewl.git",
+      repositoryBranch: 'develop',
+      repositoryComponentPath: "component-library/app/components"
     }
 
     try {
@@ -27,7 +31,25 @@ export default class Init extends Command {
     const defaultPath = "app/components"
     const componentPath = await cli.prompt(`Relative component path?`, {default: config.componentPath})
 
-    fs.writeFileSync(CONFIG_FILE_NAME, JSON.stringify({componentPath}))
-    this.log(`Jewl initialized, "${CONFIG_FILE_NAME}" created/updated in current working directory. This file should be checked in to your repository`)
+    fs.writeFileSync(CONFIG_FILE_NAME, JSON.stringify({...config, componentPath}))
+
+    try {
+      const repo: Repository = await Repository.open(REPO_PATH)
+      this.log(`Local repository exists, getting latest changes...`)
+      repo.fetch("origin")
+      this.log(`Local repository updated!`)
+    } catch (e) {
+      try {
+        this.log(`No local repository found. Cloning from remote repository...`)
+        const repository: Repository = await Clone.clone(config.repository, REPO_PATH, {
+          checkoutBranch: config.repositoryBranch,
+        })
+        this.log(`Jewl initialized: "${CONFIG_FILE_NAME}" has been created/updated in current working directory and local repository updated.`)
+        this.log(`${CONFIG_FILE_NAME} should be check in to you repository and ${REPO_PATH} should be in your .gitignore`)
+      } catch (e) {
+        this.error("Failed to clone repository: " + e.message)
+      }
+
+    }
   }
 }
--- cli/src/constants.ts
+++ cli/src/constants.ts
@@ -1,1 +1,2 @@
 export const CONFIG_FILE_NAME = 'jewlconfig.json'
+export const REPO_PATH = '.jewl-repo'
