b2bed041dd21639f4da2951b9a973414efc321d1
saraojelind <sara.ojelind@frojd.se>
2022-01-27 12:06:28 +0100

    Update styleguide and rawhtml

--- component-library/app/components/RawHtml/RawHtml.data.js
+++ component-library/app/components/RawHtml/RawHtml.data.js
@@ -1,5 +1,47 @@
 // prettier-ignore
 export default {
-    html:
-        '<h1>H1 Title</h1><p>Paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <strong>Strong ut enim ad</strong> minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <em>EM Excepteur sint occaecat</em> cupidatat non <a href=\'#\'>link here</a>, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br /> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><img src=\'https://via.placeholder.com/400x300\' alt=\'Lorem alt image\' title=\'Dolor title\'><h2>H2 Lorem ipsum dolor</h2><p>Paragraph UL starting here</p><ul><li>li Lorem</li><li>li Ipsum</li><li>li Dolor</li></ul><p>Paragraph and OL list starts here</p><ol><li>OL LI one</li><li>OL LI two</li></ol><p>Paragraph Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p><blockquote cite=\'Lorem Ipsum\'>Blockquote: Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</blockquote><h3>H3 Excepteur sint</h3><p>Paragraph Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><h4>H4 Lorem ipsum h4</h4><p>Paragraph Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</p><p>Paragraph Irure dolor in reprehenderit in voluptate <a href=\'a.pdf\'>PDF Link</a><br /> and a <a href=\'d.docx\'>Docx link</a></p>',
+    html: `
+        <h1>H1/H2 Title</h1>
+        <p>Paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <strong>Strong ut enim ad</strong> minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <em>EM Excepteur sint occaecat</em> cupidatat non <a href="#">link here</a>, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br /> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <figure>
+            <img src="https://via.placeholder.com/400x300" width="400" height="300" alt="Lorem alt image" title="Dolor title">
+        </figure>
+        <h2>H2 Lorem ipsum dolor</h2>
+        <p>Paragraph UL starting here</p>
+        <ul>
+            <li>li Lorem</li>
+            <li>li Ipsum</li>
+            <li>li Dolor</li>
+        </ul>
+        <h3>OL list starts here</h3>
+        <ol>
+            <li>OL LI one</li>
+            <li>OL LI two</li>
+        </ol>
+        <figure>
+            <img src="https://via.placeholder.com/400x300" width="400" height="300" alt="Lorem alt image" title="Dolor title">
+            <figcaption>Image caption...</figcaption>
+        </figure>
+        <figure>
+            <img src="https://via.placeholder.com/1000x400" width="1000" height="400" alt="Lorem alt image" title="Dolor title">
+            <figcaption>Image caption...</figcaption>
+        </figure>
+        <p>Paragraph Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <blockquote cite="Lorem Ipsum">Blockquote: Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</blockquote>
+        <h3>H3 Excepteur sint</h3>
+        <p>Paragraph Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h4>H4 Lorem ipsum h4</h4>
+        <p>Paragraph Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</p>
+        <p>Paragraph Irure dolor in reprehenderit in voluptate <a href="a.pdf">Download Link</a> and a <a href="https://test.se">External link</a></p>
+        <h5>H5 Excepteur sint</h5>
+        <p>Paragraph Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h6>H6 Excepteur sint</h6>
+        <p>Paragraph Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h2>H2 Excepteur sint</h2>
+        <h3>H3 Excepteur sint</h3>
+        <h4>H4 Excepteur sint</h4>
+        <h5>H5 Excepteur sint</h5>
+        <h6>H6 Excepteur sint</h6>
+        <p>Paragraph Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    `,
 };
