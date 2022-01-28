c7eff986735f260e65b4d45bce9f0cd678a9809e
saraojelind <sara.ojelind@frojd.se>
2022-01-28 16:25:06 +0100

    Change githooks

--- git-hooks/pre-push.sh
+++ git-hooks/pre-push.sh
@@ -3,7 +3,7 @@
 FRONTEND_PATH="component-library"
 APP_PATH="$FRONTEND_PATH/app"
 
-cd $FRONTEND_PATH && npm run deploy-storybook
+cd $FRONTEND_PATH && npm run deploy
 if [ $? -ne 0 ]; then
     echo "Deploy failed"
     exit 1
