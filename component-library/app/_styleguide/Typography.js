3bc04752ee6526ee001910ae9703e5d970c642c9
saraojelind <sara.ojelind@frojd.se>
2022-01-28 16:38:22 +0100

    Change styleguide style

--- component-library/app/_styleguide/Typography.js
+++ component-library/app/_styleguide/Typography.js
@@ -8,11 +8,31 @@ const Typography = () => {
 
                 <h2>Headings</h2>
 
-                <pre>@include textstyle(h1);</pre>
-                <div className="textstyle textstyle--h1">Heading h1</div>
+                <pre>@include textstyle(title);</pre>
+                <div className="textstyle textstyle--title">Title</div>
 
-                <pre>@include textstyle(h2);</pre>
-                <div className="textstyle textstyle--h2">Heading h2</div>
+                <pre>@include textstyle(module-title);</pre>
+                <div className="textstyle textstyle--module-title">Module title</div>
+
+                <pre>@include textstyle(card-title);</pre>
+                <div className="textstyle textstyle--card-title">Card title</div>
+
+                <h2>HTML headings</h2>
+
+                <pre>@include textstyle(html-h2);</pre>
+                <div className="textstyle textstyle--html-h2">Heading h2</div>
+
+                <pre>@include textstyle(html-h3);</pre>
+                <div className="textstyle textstyle--html-h3">Heading h3</div>
+
+                <pre>@include textstyle(html-h4);</pre>
+                <div className="textstyle textstyle--html-h4">Heading h4</div>
+
+                <pre>@include textstyle(html-h5);</pre>
+                <div className="textstyle textstyle--html-h5">Heading h5</div>
+
+                <pre>@include textstyle(html-h6);</pre>
+                <div className="textstyle textstyle--html-h6">Heading h6</div>
 
                 <h2>Body</h2>
 
