58c6a07c7324afc14d0d8b73a9c6c27af015c27f
saraojelind <sara.ojelind@frojd.se>
2022-08-16 10:07:45 +0200

    Add controls to button

--- component-library/app/components/Button/Button.stories.js
+++ component-library/app/components/Button/Button.stories.js
@@ -7,22 +7,50 @@ export default {
     component: Button,
     parameters: {
         docs: {description: {component: readme}},
+    },
+    argTypes: {
         modifier: {
+            defaultValue: 'Primary',
             options: ['Primary', 'Secondary', 'Tertiary', 'TextLink', 'TextLinkWhite'],
             control: {type: 'select'},
         },
-        // size: {
-        //     options: ['Large'],
-        //     control: {type: 'select'},
-        // },
+        size: {
+            defaultValue: 'Large',
+            options: ['Large'],
+            control: {type: 'select'},
+        },
         icon: {
+            defaultValue: '',
             options: ['', 'Arrow', 'External', 'Download'],
             control: {type: 'select'},
         },
     },
 };
 
-const Template = (args) => <Button {...args} />;
+const Template = (args) => {
+    console.log(args);
+    const style = {
+        padding: '20px',
+        backgroundColor: args.modifier === 'TextLinkWhite' ? 'rgba(0,0,0,.8)' : '',
+    };
+    return (
+        <div style={style}>
+            <Button {...args} />
+        </div>
+    );
+};
 
-export const Default = Template.bind({});
-Default.args = {...data};
+export const Primary = Template.bind({});
+Primary.args = {...data, modifier: 'Primary'};
+
+export const Secondary = Template.bind({});
+Secondary.args = {...data, modifier: 'Secondary'};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {...data, modifier: 'Tertiary'};
+
+export const TextLink = Template.bind({});
+TextLink.args = {...data, modifier: 'TextLink'};
+
+export const TextLinkWhite = Template.bind({});
+TextLinkWhite.args = {...data, modifier: 'TextLinkWhite'};
