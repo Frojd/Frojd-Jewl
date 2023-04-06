81bd3024dc07f38314d59ff724d971ad33b67b7d
Fanny <fanny@Fannys-MacBook-Pro.local>
2023-04-06 09:52:46 +0200

    Update searchpage with dummy data for result list

--- component-library/app/containers/SearchPage/SearchPage.data.js
+++ component-library/app/containers/SearchPage/SearchPage.data.js
@@ -1,5 +1,64 @@
 import base from 'Layouts/Base/Base.data';
 
 export default {
-    ...base,
+    ...base, //TODO: remove breadcrumbs
+    searchterm: 'Ord Att Söka Efter', 
+    filters: [
+        {
+            label: 'Label 1',
+            value: '', 
+        },
+        {
+            label: 'Label 2',
+            value: '', 
+        },
+        {
+            label: 'Label 3',
+            value: '', 
+        },
+    ],
+    resultlist: [
+        {
+            title: 'Card title',
+            url: 'www.google.se',
+            label: 'Label',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam adipiscing. Duis vel nibh at velit scelerisque suscipit.',
+            // TODO: figure out how to add image: image,
+            dateString: '2022-02-03',
+            dateFormatted: '3 februari 2022',
+        },
+        {
+            title: 'Här testar vi en lite längre rubrik för ett nyhetskort',
+            url: 'www.google.se',
+            label: 'Label',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam adipiscing. Duis vel nibh at velit scelerisque suscipit.',
+            // image: image,
+            dateString: '2022-02-03',
+            dateFormatted: '3 februari 2022',
+        },
+        {
+            title: 'Card title',
+            url: 'www.google.se',
+            label: 'Label',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam adipiscing. Duis vel nibh at velit scelerisque suscipit.',
+            dateString: '2022-02-03',
+            dateFormatted: '3 februari 2022',
+        },
+        {
+            title: 'Card title',
+            url: 'www.google.se',
+            label: 'Label',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam adipiscing. Duis vel nibh at velit scelerisque suscipit.',
+            dateString: '2022-02-03',
+            dateFormatted: '3 februari 2022',
+        },
+        {
+            title: 'Här testar vi en lite längre rubrik för ett nyhetskort',
+            url: 'www.google.se',
+            label: 'Label',
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam adipiscing. Duis vel nibh at velit scelerisque suscipit.',
+            dateString: '2022-02-03',
+            dateFormatted: '3 februari 2022',
+        },
+    ]
 };
