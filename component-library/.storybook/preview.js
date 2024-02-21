b91f6ef7cec9e45d9fab558ec6d1684e09b0349b
saraojelind <sara.ojelind@frojd.se>
2024-02-21 16:37:45 +0100

    Fix i18n in storybook

--- component-library/.storybook/preview.js
+++ component-library/.storybook/preview.js
@@ -1,13 +1,32 @@
-import 'i18n';
+import React from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'i18n';
 import './storybook.scss';
 
-export const parameters = {
-    layout: 'fullscreen',
-    html: {
-        prettier: {
-            tabWidth: 4,
-            useTabs: false,
-            htmlWhitespaceSensitivity: 'css',
+const withProvider = (Story, context) => {
+    return (
+        <div>
+            <I18nextProvider i18n={i18n}>
+                <Story
+                    {...context}
+                />
+            </I18nextProvider>
+        </div>
+    );
+};
+
+export default {
+    parameters: {
+        layout: 'fullscreen',
+        html: {
+            prettier: {
+                tabWidth: 4,
+                useTabs: false,
+                htmlWhitespaceSensitivity: 'css',
+            },
         },
     },
+    decorators: [
+        withProvider,
+    ],
 };
