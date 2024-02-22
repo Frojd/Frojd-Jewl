70bda701d271b438f68245c4a4fa94b09c60d58f
saraojelind <sara.ojelind@frojd.se>
2024-02-22 10:21:24 +0100

    Change eslint and prettier configs

--- component-library/.prettierrc.js
+++ component-library/.prettierrc.js
@@ -2,6 +2,6 @@ module.exports = {
     trailingComma: "es5",
     tabWidth: 4,
     singleQuote: true,
-    jsxBracketSameLine: true,
-    arrowParens: "always"
+    bracketSameLine: false,
+    arrowParens: "always",
 };
