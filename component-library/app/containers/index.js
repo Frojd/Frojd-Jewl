794775e5b204a14d77d063bd4b852fff75e6e30d
saraojelind <sara.ojelind@frojd.se>
2024-02-27 17:35:39 +0100

    Fix lint error for anonymous export

--- component-library/app/containers/index.js
+++ component-library/app/containers/index.js
@@ -1,8 +1,10 @@
 import dynamic from 'next/dynamic';
 
-export default {
+const Containers = {
     Landing: dynamic(() => import('./Landing')),
     NotFound: dynamic(() => import('./NotFound')),
     Page: dynamic(() => import('./Page')),
     Search: dynamic(() => import('./Search')),
 };
+
+export default Containers;
