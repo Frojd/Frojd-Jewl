bdc6e8d7a056544e0f8ccbe874932c4e55055457
saraojelind <sara.ojelind@frojd.se>
2024-02-28 09:35:38 +0100

    Add prettier check to pre commit

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -6,7 +6,13 @@ git diff --cached --name-only | if grep -e $APP_PATH
 then
     cd $FRONTEND_PATH && npm run test
     if [ $? -ne 0 ]; then
-        echo "Lint failed, please check your code and try again."
+        echo "Test failed, please check your code and try again."
+        exit 1
+    fi
+
+    npm run fixcode:test
+    if [ $? -ne 0 ]; then
+        echo "Prettier failed, please check your code and try again."
         exit 1
     fi
 fi
