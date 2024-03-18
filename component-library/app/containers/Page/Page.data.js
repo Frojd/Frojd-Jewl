348ad89b9d5a728615a13f7090f8e3686377cd6e
saraojelind <sara.ojelind@frojd.se>
2024-03-18 09:00:39 +0100

    Add Image as featured to page and fix style

--- component-library/app/containers/Page/Page.data.js
+++ component-library/app/containers/Page/Page.data.js
@@ -1,10 +1,12 @@
 import base from 'Layouts/Base/Base.data';
+import image from 'Components/Image/Image.data';
 import richtext from 'Components/Richtext/Richtext.data';
 import submenu from 'Components/Submenu/Submenu.data';
 
 const data = {
     ...base,
     title: 'A headline for an exciting news article',
+    image: image,
     content: richtext,
     submenu: submenu,
 };
--- component-library/app/containers/Page/Page.js
+++ component-library/app/containers/Page/Page.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Base from 'Layouts/Base';
+import Image from 'Components/Image';
 import Richtext from 'Components/Richtext';
 import Submenu from 'Components/Submenu';
 import s from './Page.module.scss';
 
-const Page = ({ title = '', content = {}, submenu = {} }) => {
+const Page = ({ title = '', image = {}, content = {}, submenu = {} }) => {
+    const hasFeatured = !!image;
+
     return (
         <article className={s.Root}>
             <div className={s.Wrap}>
-                <div className={s.Layout}>
-                    <div className={s.Content}>
+                <div className={s.HeaderWrap}>
+                    <div className={s.Header}>
                         <h1
                             className={s.Title}
                             dangerouslySetInnerHTML={{ __html: title }}
                         />
+                    </div>
+                </div>
 
+                {hasFeatured && (
+                    <div className={s.Featured}>
+                        <Image {...image} useCover={false} />
+                    </div>
+                )}
+
+                <div className={s.Layout}>
+                    <div className={s.Content}>
                         <Richtext {...content} />
                     </div>
 
