694c7da3ca28323ceeaa646d77583b25afc1693f
Mikael Engström <mikael.pj.engstrom@gmail.com>
2024-05-02 12:40:30 +0200

    Remove container/layout flag. Just scan for first hit instead

--- cli/src/commands/clone.ts
+++ cli/src/commands/clone.ts
@@ -11,7 +11,6 @@ import {
   getLocalComponentPath,
   getRepositoryComponentPath,
 } from '../utils/config'
-import * as fs from "fs";
 
 export default class Clone extends Command {
   static args = {
@@ -24,26 +23,18 @@ export default class Clone extends Command {
   static examples = [
     '$ jewl clone Header',
     '$ jewl clone Header HeaderLarge',
-    '$ jewl clone NotFound NotFoundPage --container',
-    '$ jewl clone Base BasePage --layout',
   ]
 
   static flags = {
-    container: Flags.boolean(),
     help: Flags.help(),
-    layout: Flags.boolean(),
   }
 
   async run() {
-    const {args, flags} = await this.parse(Clone)
+    const {args} = await this.parse(Clone)
     const {componentName, newName} = args
 
-    const directoryName = flags.container ? 'containers' : (
-      flags.layout ? 'layouts' : 'components'
-    );
-
     try {
-      await this.clone(directoryName, componentName, newName ? newName : componentName)
+      await this.clone(componentName, newName ? newName : componentName)
     } catch (error) {
       if (error instanceof LocalConfigMissing) {
         this.error('Jewl not initialized. Try `jewl init`')
@@ -53,13 +44,23 @@ export default class Clone extends Command {
     }
   }
 
-  private async clone(directoryName:string, componentName: string, newName: string) {
+  private async clone(componentName: string, newName: string) {
+    let directoryName = "";
+
+    for (const x of ["components", "containers", "layouts"]) {
+      if (getAvailableComponents(x).filter((name: string) => name === componentName).length !== 0) {
+        directoryName = x
+        break
+      }
+    }
+
+    if (!directoryName) {
+      this.error(`No component named ${componentName} found in local repository. Try "jewl list" to see available components`)
+    }
+
     const componentAbsPath = path.join(getRepositoryComponentPath(directoryName), componentName)
     const componentDestinationAbsPath = path.join(getLocalComponentPath(directoryName), newName)
 
-    if (getAvailableComponents(directoryName).filter((name: string) => name === componentName).length === 0) {
-      this.error(`No component named ${componentName} found in local repository. Try "jewl list" to see available components`)
-    }
 
     const _package = await fse.readJson(path.join(componentAbsPath, 'package.json')).catch(error => {
       this.error(`Component "${componentName}" package.json file unreadable or non existing: ` + error)
@@ -100,7 +101,7 @@ export default class Clone extends Command {
           '. After the installation is complete, make sure to update this components imports and usages if needed')
       } else {
         this.log(`Installing Jewl dependency "${dep}"...`)
-        await this.clone(depDir, depName, depName)
+        await this.clone(depName, depName)
       }
     })
   }
