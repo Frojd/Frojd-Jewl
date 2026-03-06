f2197340768b77719f9cc74c2c8115c58d2a877a
saraojelind <sara.ojelind@frojd.se>
2026-03-06 13:30:30 +0100

    Change esline config

--- component-library/eslint.config.js
+++ component-library/eslint.config.js
@@ -1,7 +1,9 @@
 import js from '@eslint/js';
+import globals from 'globals';
 import storybookPlugin from 'eslint-plugin-storybook';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import nextPlugin from '@next/eslint-plugin-next';
 
 export default [
   js.configs.recommended,
@@ -11,7 +13,10 @@ export default [
       '.next/**',
       'storybook-static/**',
       'cli/**',
-      'dist/**'
+      'dist/**',
+      '**/__Component__/**',
+      '**/__Container__/**',
+      '**/__Layout__/**',
     ]
   },
   {
@@ -25,29 +30,17 @@ export default [
         }
       },
       globals: {
-        window: 'readonly',
-        document: 'readonly',
-        navigator: 'readonly',
-        console: 'readonly',
-        module: 'readonly',
-        require: 'readonly',
-        process: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly',
-        React: 'readonly',
-        describe: 'readonly',
-        it: 'readonly',
-        expect: 'readonly',
-        test: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        jest: 'readonly',
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.jest,
+        process: 'readonly', // Next.js makes process.env available in browser
       }
     },
     plugins: {
       react: reactPlugin,
       'react-hooks': reactHooksPlugin,
       storybook: storybookPlugin,
+      '@next/next': nextPlugin,
     },
     rules: {
       'react/react-in-jsx-scope': 'off',
@@ -55,6 +48,7 @@ export default [
       'react/jsx-uses-vars': 'error',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
+      '@next/next/no-img-element': 'warn',
       'no-unused-vars': ['error', {
         varsIgnorePattern: '^React$',
         args: 'after-used',
