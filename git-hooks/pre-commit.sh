31828f38fa59861a9d58b873441b6f5ed9acd22a
saraojelind <sara.ojelind@frojd.se>
2026-03-12 18:33:12 +0100

    Change pre commit hook

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -5,9 +5,12 @@ CLI_PATH="cli"
 APP_PATH="$FRONTEND_PATH/app"
 
 # Check if component-library app files changed
-git diff --cached --name-only | if grep -e $APP_PATH
+if git diff --cached --name-only | grep -q -e "$APP_PATH"
 then
-    cd $FRONTEND_PATH && npm run test
+    echo "Running component library tests..."
+    cd "$FRONTEND_PATH" || exit 1
+
+    npm run test
     if [ $? -ne 0 ]; then
         echo "Component library tests failed, please check your code and try again."
         exit 1
@@ -20,17 +23,29 @@ then
     fi
 
     cd ..
-    cd $CLI_PATH
+    cd "$CLI_PATH" || exit 1
     npm run update_docs
+
+    cd ..
 fi
 
 # Check if CLI files changed
-git diff --cached --name-only | if grep -e "^$CLI_PATH/src" -e "^$CLI_PATH/test"
+if git diff --cached --name-only | grep -q -e "^$CLI_PATH/src" -e "^$CLI_PATH/test"
 then
     echo "Running CLI tests..."
-    cd $CLI_PATH && npm run build && npm run test:only
+    cd "$CLI_PATH" || exit 1
+
+    npm run build
+    if [ $? -ne 0 ]; then
+        echo "CLI build failed, please check your code and try again."
+        exit 1
+    fi
+
+    npm run test:only
     if [ $? -ne 0 ]; then
         echo "CLI tests failed, please check your code and try again."
         exit 1
     fi
+
+    cd ..
 fi
