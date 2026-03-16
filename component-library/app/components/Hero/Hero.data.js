996e5583f7ffddf660bfcdb4808db05dc1d73bac
frojd-sandra-host-kannerberg <sandra.host.kannerberg@frojd.se>
2026-03-16 14:27:13 +0100

    Add image, preamble and button to Hero component

--- component-library/app/components/Hero/Hero.data.js
+++ component-library/app/components/Hero/Hero.data.js
@@ -1,5 +1,12 @@
+import image from '../Image/Image.data';
+import button from '../Button/Button.data';
+
 const data = {
-    title: 'Hero title',
+    title: 'Welcome to explore the Jewl project!',
+    image: image,
+    preamble:
+        'Here you will find a collection of simple components, modules, and page templates. Reuse what you want and feel free to modify the design and CSS to fit your project.',
+    button: button,
 };
 
 export default data;
