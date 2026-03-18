d576a28c1e5bfe50546d790e83d543396fb20569
frojd-sandra-host-kannerberg <sandra.host.kannerberg@frojd.se>
2026-03-18 10:59:53 +0100

    Add ContentBlockquote component

---
+++ component-library/app/components/ContentBlockquote/ContentBlockquote.data.js
@@ -0,0 +1,6 @@
+const data = {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada nulla non nisi auctor tempor.',
+    cite: 'Author Name',
+};
+
+export default data;
